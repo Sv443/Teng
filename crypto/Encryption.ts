817779ede6954cddd16deb57e569defd479b8676
Sven <svenfehler@sv443.net>
2021-04-30 12:44:12 +0200

    added raw methods to Encryption class

--- crypto/Encryption.ts
+++ crypto/Encryption.ts
@@ -14,39 +14,64 @@ const algorithm: CipherCCMTypes | CipherGCMTypes = "aes-256-gcm";
 export default abstract class Encryption
 {
     /**
-     * Encrypts some data with AES, using a provided encryption key.  
+     * Encrypts a string with AES, using a provided encryption key.  
      * To convert the returned Buffer to a string or JSON object, use `.toString()` or `.toJSON()`
      * @returns Returns a buffer containing the encrypted data
      */
     static encrypt(data: string, key: string): Buffer
     {
+        return Encryption.encryptRaw(Buffer.from(data), key);
+    }
+
+    /**
+     * Encrypts a raw data (a Buffer instance) with AES, using a provided encryption key.  
+     * To convert the returned Buffer to a string or JSON object, use `.toString()` or `.toJSON()`
+     * @returns Returns a buffer containing the encrypted data
+     */
+    static encryptRaw(data: Buffer, key: string): Buffer
+    {
         const keyHash = createHash("md5").update(key).digest("hex");
         const iv = randomBytes(tengSettings.crypto.initVectorLength);
 
         const cipher = createCipheriv(algorithm, Buffer.from(keyHash), iv);
-        const encrypted = cipher.update(data.toString());
+        const encrypted = cipher.update(data);
 
         return Buffer.concat([iv, encrypted, cipher.final()]);
     }
 
     /**
      * Tries to decrypt some data that has been encrypted using AES, using the provided encryption key.  
-     * If your encrypted data is in string format, use `Buffer.from(encryptedDataAsString)`
+     * Supports raw data (as a Buffer instance) and string data.
+     * @returns Returns the decrypted data as a string
      */
-    static decrypt(encryptedData: Buffer, key: string): string
+    static decrypt(encryptedData: Buffer | string, key: string): string
     {
-        // TODO: this shit don't work
+        return Encryption.decryptRaw(encryptedData, key).toString();
+    }
 
+    /**
+     * Tries to decrypt some data that has been encrypted using AES, using the provided encryption key.  
+     * Supports raw data (as a Buffer instance) and string data.
+     * @returns Returns the raw decrypted data (as a Buffer instance)
+     */
+    static decryptRaw(encryptedData: Buffer | string, key: string): Buffer
+    {
         const keyHash = createHash("md5").update(key).digest("hex");
 
         const iv = encryptedData.slice(0, tengSettings.crypto.initVectorLength);
         const encData = encryptedData.slice(tengSettings.crypto.initVectorLength);
 
         const decipher = createDecipheriv(algorithm, Buffer.from(keyHash), iv);
-        const decrypted = decipher.update(encData);
+        const decrypted = decipher.update((encData instanceof Buffer) ? encData : Buffer.from(encData));
 
-        decipher.final();
+        // using Decipher.final() somehow crashes??
+        // Even tho the documentation says I should be able to????? - https://nodejs.org/api/crypto.html#crypto_decipher_final_outputencoding
+        // What is this shit???????????????
+        //
+        // decipher.final();
 
-        return decrypted.toString();
+        decipher.end();
+
+        return decrypted;
     }
 }
